593a619ea11cb5c691e71b8496f42c8077291ff9
nicolasraffray <nraffray@gmail.com>
2020-05-12 17:01:39 +0100

    performance graphs rendering in index.html

---
+++ jasmine-3.5.0/spec/timingSpec.js
@@ -0,0 +1,9 @@
+
+
+describe('Timing', () => {
+  it('takes an input and returns it', () => {
+    let Time = new Timing(reverse)
+    Time.iterateFunction(100, 10000, shuffled = true)
+    console.log(Time.times)
+  })
+})
---
+++ jasmine-3.5.0/src/timeing.js
@@ -0,0 +1,65 @@
+class Timing {
+  constructor(callback){
+    this.startTime = 0
+    this.times = []
+    this.function = callback
+  }
+
+  runFunction = (inputLength, input) => {
+    let start = performance.now()
+    this.function(input)
+    let end = performance.now()
+    this.setTime(inputLength, start, end)
+  }
+
+  setTime = (iteration,start,end) => {
+    this.times.push({input: iteration, time: (end - start) })
+  }
+
+  iterateFunction = (step, endPoint) => {
+    for(let i = 0; i <= endPoint; i += step){
+      var input = [...Array(i).keys()]
+      input = this.shuffle(input)
+      this.runFunction(i, input)
+    }
+  }
+
+  shuffle = (array) => {
+    if(this.function === sort){
+      console.log("in the shuffle")
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); 
+        [array[i], array[j]] = [array[j], array[i]];
+    }}
+    return array
+  }
+}
+
+const reverse = (input) => {
+  input.reverse()
+}
+
+const sort = (input) => {
+  input.sort()
+}
+
+const last = (input) => {
+  input[input.length - 1]
+}
+
+
+const loop = (input) => {
+  for(let i = 0; i < input; i++){
+    console.log(i)
+  }
+}
+
+const shuffle = (array) => {
+  if(this.function === sort){
+    console.log("in the shuffle")
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]];
+  }}
+  return array
+}
