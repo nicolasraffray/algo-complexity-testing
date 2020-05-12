9def29f271635c06e6b3330bd3cde493233ea796
nicolasraffray <nraffray@gmail.com>
2020-05-12 17:14:45 +0100

    console.log removed

--- jasmine-3.5.0/src/timeing.js
+++ jasmine-3.5.0/src/timeing.js
@@ -56,7 +56,6 @@ const loop = (input) => {
 
 const shuffle = (array) => {
   if(this.function === sort){
-    console.log("in the shuffle")
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1)); 
       [array[i], array[j]] = [array[j], array[i]];
