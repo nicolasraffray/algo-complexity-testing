e7ca4fd239f86c431d8e63c89d318bed974c62ff
nicolasraffray <nraffray@gmail.com>
2020-05-20 15:58:32 +0100

    chart class made

---
+++ jasmine-3.5.0/src/chart.js
@@ -0,0 +1,61 @@
+class MakeChart {
+  constructor(name) {
+    this.chartName = name;
+    this.functions = {
+      sort: new Sort(),
+      reverse: new Reverse(),
+    };
+  }
+
+  _generateOptions = () => {
+    return (options = {
+      scales: {
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Elements in Array",
+            },
+          },
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Milliseconds",
+            },
+          },
+        ],
+      },
+    });
+  };
+
+  generate = () => {
+    this.functions[this.chartName].run();
+    var ctx = document.getElementById(this.chartName).getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: this.functions[this.chartName].labels,
+        datasets: [
+          {
+            label: this.chartName,
+            data: this.functions[this.chartName].dataset,
+            fill: false,
+            backgroundColor: "#fbbd08",
+            borderColor: "#fbbd08",
+            borderWidth: 2,
+            pointBackgroundColor: "#fbbd08",
+            pointBorderWidth: 0.5,
+            pointStyle: "rectRounded",
+            pointRadius: 4,
+            pointHitRadius: 5,
+            pointHoverRadius: 5,
+            hoverBackgroundColor: "#FFFFFF",
+          },
+        ],
+      },
+      options: this._generateOptions(),
+    });
+  };
+}
