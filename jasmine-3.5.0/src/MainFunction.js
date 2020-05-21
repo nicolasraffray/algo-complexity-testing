78f0f4aba5689fea32eb5e9a6eb0ffa563faf446
nicolasraffray <nraffray@gmail.com>
2020-05-21 21:52:47 +0100

    class to extend other classes from

---
+++ jasmine-3.5.0/src/MainFunction.js
@@ -0,0 +1,27 @@
+class MainFunction {
+  constructor(step, end) {
+    this.timing = [];
+    this.dataset = [];
+    this.labels = [];
+    this.step = step;
+    this.end = end;
+  }
+
+  _generateData = () => {
+    this.timing.iterateFunction(this.step, this.end);
+    this.timing.times.forEach((iteration) => {
+      this.dataset.push(iteration.time);
+    });
+    this._generateLabels();
+  };
+
+  _generateLabels = () => {
+    this.timing.times.forEach((iteration) => {
+      this.labels.push(iteration.input);
+    });
+  };
+
+  run = () => {
+    this._generateData();
+  };
+}
