9ae968e8d3e7334ee37ae9c9b2d99b095119c009
nicolasraffray <nraffray@gmail.com>
2020-05-27 10:47:17 +0100

    timing function can take n number of functions inside arry

--- jasmine-3.5.0/src/timeing.js
+++ jasmine-3.5.0/src/timeing.js
@@ -3,24 +3,43 @@ class Timing {
     this.startTime = 0;
     this.times = [];
     this.function = callback;
+    this.dataStore = [];
   }
 
-  _runFunction = (inputLength, input) => {
+  _runFunction = (inputLength, input, theFunction, dataStore) => {
     let start = performance.now();
-    this.function(input);
+    theFunction(input);
     let end = performance.now();
-    this._setTime(inputLength, start, end);
+    this._setTime(inputLength, start, end, dataStore);
   };
 
-  _setTime = (iteration, start, end) => {
-    this.times.push({ input: iteration, time: end - start });
+  _setTime = (iteration, start, end, dataStore) => {
+    dataStore.push({ input: iteration, time: end - start });
   };
 
   iterateFunction = (step, endPoint) => {
+    if (typeof this.function === "function") {
+      this._loopFromStartToFinish(step, endPoint, this.function, this.times);
+    } else {
+      for (let func in this.function) {
+        this.dataStore = [];
+        this._loopFromStartToFinish(
+          step,
+          endPoint,
+          this.function[func],
+          this.dataStore
+        );
+        this.times.push(this.dataStore);
+      }
+    }
+    console.log(JSON.stringify(this.function), this.times);
+  };
+
+  _loopFromStartToFinish = (step, endPoint, theFunction, dataStore) => {
     for (let i = 0; i <= endPoint; i += step) {
       var input = [...Array(i).keys()];
       input = this._shuffle(input);
-      this._runFunction(i, input);
+      this._runFunction(i, input, theFunction, dataStore);
     }
   };
 
@@ -35,12 +54,3 @@ class Timing {
     return array;
   };
 }
-
-const shuffle = (array) => {
-  if (this.function === sort) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-};
