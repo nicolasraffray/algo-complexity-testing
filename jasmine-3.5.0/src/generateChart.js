8cc66fe9afc9cc5393d4113de1e2e08efc64d3c1
nicolasraffray <nraffray@gmail.com>
2020-06-10 20:59:19 +0100

    extracting charts from index html into thier own file

---
+++ jasmine-3.5.0/src/generateChart.js
@@ -0,0 +1,8 @@
+var sortChart = new MakeChart("sort", 100, 10000);
+var reverseChart = new MakeChart("reverse", 100, 10000);
+var shuffleChart = new MakeChart("shuffle", 100, 10000);
+var fibonnaci = new MakeChart("fibbonaci");
+shuffleChart.generate();
+sortChart.generate();
+reverseChart.generate();
+fibonnaci.generate();
