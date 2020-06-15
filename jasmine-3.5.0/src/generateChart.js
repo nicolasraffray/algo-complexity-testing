abecd23290b019edcb0bc9edd70fc1ae7c5d5f9c
nicolasraffray <nraffray@gmail.com>
2020-06-15 20:55:36 +0100

    quick refactor on generating chart

--- jasmine-3.5.0/src/generateChart.js
+++ jasmine-3.5.0/src/generateChart.js
@@ -1,8 +1,8 @@
-var sortChart = new MakeChart("sort", 100, 10000);
-var reverseChart = new MakeChart("reverse", 100, 10000);
-var shuffleChart = new MakeChart("shuffle", 100, 10000);
-var fibonnaci = new MakeChart("fibonacci", 100, 10000);
-shuffleChart.generate();
-sortChart.generate();
-reverseChart.generate();
-fibonnaci.generate();
+let functions = ["sort", "reverse", "shuffle", "fibonacci"];
+let chart = functions.map((func) => {
+  return new MakeChart(func, 100, 10000);
+});
+console.log(chart);
+chart.forEach((chart) => {
+  return chart.generate();
+});
