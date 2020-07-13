846c067fa57ae3eac86fbb4c3ff53b422ef72866
nicolasraffray <nraffray@gmail.com>
2020-07-13 16:44:57 +0100

    shuffle method added

--- jasmine-3.5.0/src/shuffle.js
+++ jasmine-3.5.0/src/shuffle.js
@@ -1,17 +1,20 @@
 class Shuffle {
   constructor() {
     this.timing = new Timing([this.theShuffle, this.jsShuffle]);
-    this.functionNames = ["Shuffle", "JavaScript Sort"];
+    this.functionNames = ["My Shuffle", "Shuffle, Fisher-Yates"];
   }
 
   jsShuffle = (array) => {
-    array.sort();
-  };
-
-  theShuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
+  };
+
+  theShuffle = (array) => {
+    let shuffled = array
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value);
   };
 }
