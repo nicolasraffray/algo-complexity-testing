ad8418d309d7e7518db46e19f56d8f9f2c66176a
nicolasraffray <nraffray@gmail.com>
2020-07-13 21:38:00 +0100

    adding a reverse function

--- jasmine-3.5.0/src/reverse.js
+++ jasmine-3.5.0/src/reverse.js
@@ -1,10 +1,20 @@
 class Reverse {
   constructor() {
-    this.timing = new Timing(this._reverse);
-    this.functionNames = ["JavaScript Reverse"];
+    this.timing = new Timing([this._reverse, this.myReverse]);
+    this.functionNames = ["JavaScript Reverse", "My While Iteration Reverse"];
   }
 
   _reverse = (input) => {
-    input.reverse();
+    console.log(input);
+    return input.reverse();
+  };
+
+  myReverse = (input) => {
+    console.log(input);
+    var result = [];
+    while (input.length > 1) {
+      result.push(input.pop());
+    }
+    return result;
   };
 }
