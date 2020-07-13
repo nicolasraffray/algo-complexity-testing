65502f3398ea9d826bc905b96756f255b1f75cb6
nicolasraffray <nraffray@gmail.com>
2020-07-13 21:14:53 +0100

    merge sort added

--- jasmine-3.5.0/src/sort.js
+++ jasmine-3.5.0/src/sort.js
@@ -1,7 +1,10 @@
 class Sort {
   constructor() {
-    this.timing = new Timing([this.sort, this.quickSort], "Sort");
-    this.functionNames = ["JavaScript Sort", "QuickSort"];
+    this.timing = new Timing(
+      [this.sort, this.quickSort, this.mergeSort],
+      "Sort"
+    );
+    this.functionNames = ["JavaScript Sort", "QuickSort", "MergeSort"];
   }
 
   sort = (input) => {
@@ -12,16 +15,38 @@ class Sort {
     if (input.length <= 1) {
       return input;
     }
-
     let pivot = input[0];
-
     let low = [];
     let high = [];
-
     for (let i = 1; i < input.length; i++) {
       input[i] < pivot ? low.push(input[i]) : high.push(input[i]);
     }
-
     return this.quickSort(low).concat(pivot, this.quickSort(high));
+  };
+
+  mergeSort = (input) => {
+    if (input.length < 2) return input;
+
+    let mid = Math.floor(input.length / 2);
+    let left = input.slice(0, mid);
+    let right = input.slice(mid);
+
+    const merge = (left, right) => {
+      let result = [];
+      let leftInd = 0,
+        rightInd = 0;
+
+      while (leftInd < left.length && rightInd < right.length) {
+        if (left[leftInd] < right[rightInd]) {
+          result.push(left[leftInd]);
+          leftInd += 1;
+        } else {
+          result.push(right[rightInd]);
+          rightInd += 1;
+        }
+      }
+      return result.concat(left.slice(leftInd)).concat(right.slice(rightInd));
+    };
+    return merge(this.mergeSort(left), this.mergeSort(right));
   };
 }
