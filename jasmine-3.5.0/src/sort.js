7448853efd74de1234a644322f86683612b46eef
nicolasraffray <nraffray@gmail.com>
2020-07-14 17:40:26 +0100

    insertion sort added

--- jasmine-3.5.0/src/sort.js
+++ jasmine-3.5.0/src/sort.js
@@ -1,10 +1,15 @@
 class Sort {
   constructor() {
     this.timing = new Timing(
-      [this.sort, this.quickSort, this.mergeSort],
+      [this.sort, this.quickSort, this.mergeSort, this.selectionSort],
       "Sort"
     );
-    this.functionNames = ["JavaScript Sort", "QuickSort", "MergeSort"];
+    this.functionNames = [
+      "JavaScript Sort",
+      "QuickSort",
+      "MergeSort",
+      "SelectionSort",
+    ];
   }
 
   sort = (input) => {
@@ -48,5 +53,19 @@ class Sort {
       return result.concat(left.slice(leftInd)).concat(right.slice(rightInd));
     };
     return merge(this.mergeSort(left), this.mergeSort(right));
+  };
+
+  selectionSort = (input) => {
+    for (let i = 0; i < input.length; i++) {
+      var check = input[i];
+
+      var pointer = i - 1;
+      while (pointer > 0 && check < input[pointer]) {
+        input[pointer + 1] = input[pointer];
+        pointer -= 1;
+      }
+      input[pointer + 1] = check;
+    }
+    console.log(input);
   };
 }
